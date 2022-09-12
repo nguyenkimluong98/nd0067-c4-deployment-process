cf1f9ce56cdcdb07593bf794f1caeec008b06c59
nguyenkimluong98 <nguyenkimluong98@gmail.com>
2022-09-12 11:39:59 +0700

    change config

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://luongnk1-udagram/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://luongnk1-udagram/
+aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-725742132730/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-725742132730/
